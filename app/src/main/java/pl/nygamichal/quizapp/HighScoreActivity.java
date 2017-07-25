e11fe9d67f85105da01e51d9e35ed46515c60c58
nygamichal <mnyga94@gmail.com>
2017-07-25 17:09:39 +0200

    sharedPreferences & done.

--- app/src/main/java/pl/nygamichal/quizapp/HighScoreActivity.java
+++ app/src/main/java/pl/nygamichal/quizapp/HighScoreActivity.java
@@ -1,8 +1,10 @@
 package pl.nygamichal.quizapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.parceler.Parcels;
 
@@ -18,12 +20,20 @@ public class HighScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
         ButterKnife.bind(this);
+        SharedPreferences sharedPreferences = getSharedPreferences("pl.nygamichal.quizapp", MODE_PRIVATE);
         if (getIntent().hasExtra("quiz"))
         {
             Quiz quiz = Parcels.unwrap(getIntent().getParcelableExtra("quiz"));
             if (quiz != null)
             {
                 textViewResult.setText(Integer.toString(quiz.correctAnswers));
+                if (quiz.correctAnswers > sharedPreferences.getInt("highscore",0))
+                {
+                    Toast.makeText(this,"Hey! New High score!",Toast.LENGTH_SHORT).show();
+                    sharedPreferences.edit().putInt("highscore",quiz.correctAnswers).apply();//commit LUB apply
+                    //commit - jest natychmiastowy, wywoalnie kolejnej lini kodu czeka, az commit sie skonczy,
+                    //apply - jest asynchroniczna i nie czeka, aż zapisze sie do sharedPreferences lecz od razu pokazuje wynik
+                }
             }
         }
     }
